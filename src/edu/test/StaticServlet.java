7fbc97d824938f6065ccdd5d38adb81543ad98a2
Aaron <aaronc95@hotmail.com>
2015-09-20 15:27:50 +1000

    modified print stmnt

--- src/edu/test/StaticServlet.java
+++ src/edu/test/StaticServlet.java
@@ -29,6 +29,7 @@ public class StaticServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.out.println("PRint something");
+		System.out.println("print something else");
 		response.setContentType("text/html");
 		PrintWriter out = response.getWriter();
 		out.println("<HTML>");
